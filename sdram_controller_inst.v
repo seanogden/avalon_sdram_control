4af954ab6fe7b732684d39b56e247d2739b24dfc
Sean Ogden <positivelyskewed.sf@gmail.com>
2015-05-06 17:43:00 -0400

    initial commit

---
+++ sdram_controller_inst.v
@@ -0,0 +1,23 @@
+  //Example instantiation for system 'sdram_controller'
+  sdram_controller sdram_controller_inst
+    (
+      .acknowledge_from_the_bridge_0 (acknowledge_from_the_bridge_0),
+      .address_to_the_bridge_0       (address_to_the_bridge_0),
+      .byte_enable_to_the_bridge_0   (byte_enable_to_the_bridge_0),
+      .clk_0                         (clk_0),
+      .read_data_from_the_bridge_0   (read_data_from_the_bridge_0),
+      .read_to_the_bridge_0          (read_to_the_bridge_0),
+      .reset_n                       (reset_n),
+      .write_data_to_the_bridge_0    (write_data_to_the_bridge_0),
+      .write_to_the_bridge_0         (write_to_the_bridge_0),
+      .zs_addr_from_the_sdram_0      (zs_addr_from_the_sdram_0),
+      .zs_ba_from_the_sdram_0        (zs_ba_from_the_sdram_0),
+      .zs_cas_n_from_the_sdram_0     (zs_cas_n_from_the_sdram_0),
+      .zs_cke_from_the_sdram_0       (zs_cke_from_the_sdram_0),
+      .zs_cs_n_from_the_sdram_0      (zs_cs_n_from_the_sdram_0),
+      .zs_dq_to_and_from_the_sdram_0 (zs_dq_to_and_from_the_sdram_0),
+      .zs_dqm_from_the_sdram_0       (zs_dqm_from_the_sdram_0),
+      .zs_ras_n_from_the_sdram_0     (zs_ras_n_from_the_sdram_0),
+      .zs_we_n_from_the_sdram_0      (zs_we_n_from_the_sdram_0)
+    );
+
